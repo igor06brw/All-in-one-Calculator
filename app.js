70dd1fb0f6e6943ce74f3a60300e0457aa686ba0
Igor Borowski <igor06brw@gmail.com>
2020-06-18 19:52:05 +0200

    Add decreasing calculation

--- app.js
+++ app.js
@@ -1,27 +1,16 @@
-// const mortgage = document.getElementById('mortgage');
-// const menu = document.querySelector('.menu');
-
-// mortgage.addEventListener('click', displayCalc);
-
-// function displayCalc() {
-//     menu.style.display="none";
-// }
-
-let debtValue = document.getElementById('debtValue');
-let debtRange = document.getElementById('debtRange');
-let termValue = document.getElementById('termValue');
-let termRange = document.getElementById('termRange');
-let interestRateValue = document.getElementById('interestRateValue');
-let interestRateRange = document.getElementById('interestRateRange');
-let displayResult = document.getElementById('displayResult');
-let checkRadio = document.getElementById('checkRadio');
-
-
-
-
-let valueFromDebt;
-let valueFromTerm;
-let valueFromInterestRate;
+let debtValue = document.getElementById('debtValue'),
+    debtRange = document.getElementById('debtRange'),
+    termValue = document.getElementById('termValue'),
+    termRange = document.getElementById('termRange'),
+    interestRateValue = document.getElementById('interestRateValue'),
+    interestRateRange = document.getElementById('interestRateRange'),
+    displayResult = document.getElementById('displayResult'),
+    checkRadio = document.getElementById('checkRadio'),
+    decreasingCheck = document.getElementById('forDescreasingMortgage'),
+    valueFromDebt,
+    valueFromTerm,
+    valueFromInterestRate;
+    // valueFromRadio;
 
 
 let debtFunc = (param) => {
@@ -76,21 +65,40 @@ interestRateRange.addEventListener('click',(value) => {
 });
 
 
+
+
+
 let calculateMortage = () => {
-
-    checkRadio.addEventListener('click', () => {
-        if(checkRadio.value) {
-
-        }
-    })
-    
     if(valueFromDebt > 0 && valueFromInterestRate > 0 && valueFromTerm > 0) {
-        let q = Number(1+(valueFromInterestRate / 100).toFixed(4) / 12);
-        let R = (valueFromDebt*(Math.pow(q, (valueFromTerm*12)))*(q-1)/((Math.pow(q, (valueFromTerm*12)))-1)).toFixed(2);
+        console.log(decreasingCheck.checked)
+        if(decreasingCheck.checked == false) {
+            let q = Number(1+(valueFromInterestRate / 100).toFixed(4) / 12);
+            let R = (valueFromDebt*(Math.pow(q, (valueFromTerm*12)))*(q-1)/((Math.pow(q, (valueFromTerm*12)))-1)).toFixed(2);
+            displayResult.value = `${R} zł`;
+        } else {
+            let q = Number(1+(valueFromInterestRate / 100).toFixed(4) / 12);
+            let R = (valueFromDebt*(Math.pow(q, (valueFromTerm*12)))*(q-1)/((Math.pow(q, (valueFromTerm*12)))-1)).toFixed(2);
+        
+            console.log(R);
+            let Rk = parseFloat(valueFromDebt / (valueFromTerm * 12));
+            
+            console.log(Rk);
 
-        displayResult.value = R + ' zł';
-    }
+            let Ro = ((valueFromDebt-0*Rk) * (valueFromInterestRate / 100)) / 12;
+
+            console.log(Rk+Ro);
+
+             // Rata odsetkowa (Ro):
+             // Ro=[(A-x*Rk)*b]/12
     
+            displayResult.value = `${Rk+Ro} zł`;
+        } 
+    }
+}
+displayResult.addEventListener('change', calculateMortage());
+
+
+   
     
     // A– wysokość kredytu
     // b– oprocentowanie w skali roku (stopy procentowe plus marża banku)
@@ -101,22 +109,7 @@ let calculateMortage = () => {
     // q=1+(4,7%/12)=1,00392
 
     // R=A*(q^n)*(q-1)/[(q^n)-1]
-
-}
-let calculateDescreasingMortage = () => {
-    if(valueFromDebt > 0 && valueFromInterestRate > 0 && valueFromTerm > 0) {
-        let q = Number(1+(valueFromInterestRate / 100).toFixed(4) / 12);
-        let R = (valueFromDebt*(Math.pow(q, (valueFromTerm*12)))*(q-1)/((Math.pow(q, (valueFromTerm*12)))-1)).toFixed(2);
-
-
-        let Rk = valueFromDebt / (valueFromTerm * 12);
-
-
-        displayResult.value = R + ' zł';
-
-
-
-        // A– wysokość kredytu
+       // A– wysokość kredytu
         // b– oprocentowanie w skali roku (stopy procentowe plus marża banku)
         // n– ilość rat do spłaty (okres kredytowania l (jak lata) pomnożony przez m, czyli 12, bo tyle jest miesięcy w roku)
         // R– to będzie wysokość naszej raty
@@ -149,12 +142,4 @@ let calculateDescreasingMortage = () => {
         // Ro=14053/12
         // Ro=1171,08
         
-    }
-
-}
-console.log( displayResult.value)
-if(displayResult.value == NaN) {
-    displayResult.value = 000
-}
-
-displayResult.addEventListener('change', calculateMortage());
+// console.log( displayResult.value)

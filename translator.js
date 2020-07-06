4dcca89454345ce3b8d34a13648da957f367b002
Igor Borowski <igor06brw@gmail.com>
2020-07-06 05:32:49 +0200

    Add translator of app in translate button

--- translator.js
+++ translator.js
@@ -12,48 +12,64 @@ const headerTitle = document.getElementById('headerTitle'),
       mortgageDecreasing = document.getElementById('forDescreasingMortgageLabel'),
       mortgageResultMin = document.getElementById('displayMinResultLabel'),
       mortgageResultMax = document.getElementById('displayMaxResultLabel'),
-      mortgageResultFixed = document.getElementById('displayFixedResultLabel')
+      mortgageResultFixed = document.getElementById('displayFixedResultLabel'),
+      translateAppButton = document.getElementById('translateAppButton')
 
-// let words;
-let requestURL = "./dictionary.json"
-let request = new XMLHttpRequest();
-request.open('GET', requestURL);
-request.responseType = 'json'
-request.send();
-request.onload = function() {
-    const words = request.response;
+let isEnglish = true;
+      console.log(isEnglish);
 
-    // translate(words);
-}
+translateAppButton.addEventListener('click', () => {
+
+    console.log(isEnglish);
+    let words;
+    let requestURL = "./dictionary.json"
+    let request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json'
+    request.send();
+    request.onload = function() {
+        words = request.response;
+        translate(words)
+    };
+})
 
 function translate(words) {
     const polishTranslateBase = words.dictionary.polish,
           englishTranslateBase = words.dictionary.english
-
-    headerTitle.textContent = polishTranslateBase.header.title;
-    headerDescription.textContent = polishTranslateBase.header.description;
-    mortgageDebtLabel.textContent = polishTranslateBase.debt.label;
-    mortgageDebtInput.placeholder = polishTranslateBase.debt.placeholder;
-    mortgageTermLabel.textContent = polishTranslateBase.term.label;
-    mortgageTermInput.placeholder = polishTranslateBase.term.placeholder;
-    mortgageInterestRateLabel.textContent = polishTranslateBase.interestRate.label;
-    mortgageInterestRateInput.placeholder = polishTranslateBase.interestRate.placeholder;
-    mortgageTermRadioYears.textContent = polishTranslateBase.radioTerm.years;
-    mortgageTermRadioMonths.textContent = polishTranslateBase.radioTerm.months;
-    mortgageFixed.textContent = polishTranslateBase.radioMortgage.fixed;
-    mortgageDecreasing.textContent = polishTranslateBase.radioMortgage.decreasing;
-    mortgageResultMin.textContent = polishTranslateBase.displayResults.first;
-    mortgageResultMax.textContent = polishTranslateBase.displayResults.last;
-    mortgageResultFixed.textContent = polishTranslateBase.displayResults.fixed;
+          console.log(isEnglish);
+    if(isEnglish == true) {
+        isEnglish = false;
+        headerTitle.textContent = polishTranslateBase.header.title;
+        headerDescription.textContent = polishTranslateBase.header.description;
+        mortgageDebtLabel.textContent = polishTranslateBase.debt.label;
+        mortgageDebtInput.placeholder = polishTranslateBase.debt.placeholder;
+        mortgageTermLabel.textContent = polishTranslateBase.term.label;
+        mortgageTermInput.placeholder = polishTranslateBase.term.placeholder;
+        mortgageInterestRateLabel.textContent = polishTranslateBase.interestRate.label;
+        mortgageInterestRateInput.placeholder = polishTranslateBase.interestRate.placeholder;
+        mortgageTermRadioYears.textContent = polishTranslateBase.radioTerm.years;
+        mortgageTermRadioMonths.textContent = polishTranslateBase.radioTerm.months;
+        mortgageFixed.textContent = polishTranslateBase.radioMortgage.fixed;
+        mortgageDecreasing.textContent = polishTranslateBase.radioMortgage.decreasing;
+        mortgageResultMin.textContent = polishTranslateBase.displayResults.first;
+        mortgageResultMax.textContent = polishTranslateBase.displayResults.last;
+        mortgageResultFixed.textContent = polishTranslateBase.displayResults.fixed;
+    } else {
+        isEnglish = true;
+        headerTitle.textContent = englishTranslateBase.header.title;
+        headerDescription.textContent = englishTranslateBase.header.description;
+        mortgageDebtLabel.textContent = englishTranslateBase.debt.label;
+        mortgageDebtInput.placeholder = englishTranslateBase.debt.placeholder;
+        mortgageTermLabel.textContent = englishTranslateBase.term.label;
+        mortgageTermInput.placeholder = englishTranslateBase.term.placeholder;
+        mortgageInterestRateLabel.textContent = englishTranslateBase.interestRate.label;
+        mortgageInterestRateInput.placeholder = englishTranslateBase.interestRate.placeholder;
+        mortgageTermRadioYears.textContent = englishTranslateBase.radioTerm.years;
+        mortgageTermRadioMonths.textContent = englishTranslateBase.radioTerm.months;
+        mortgageFixed.textContent = englishTranslateBase.radioMortgage.fixed;
+        mortgageDecreasing.textContent = englishTranslateBase.radioMortgage.decreasing;
+        mortgageResultMin.textContent = englishTranslateBase.displayResults.first;
+        mortgageResultMax.textContent = englishTranslateBase.displayResults.last;
+        mortgageResultFixed.textContent = englishTranslateBase.displayResults.fixed;
+    }
 }
-
-
-
-
-
-// fetch('./dictionary.json')
-//     .then(res => res.json())
-//     .then(data => dictionary = data)
-//     .then(() => console.log(dictionary[1]))
-
-

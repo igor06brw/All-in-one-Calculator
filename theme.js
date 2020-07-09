f6fb2dcacb228271357f96bb6522e1e28081f092
Igor Borowski <igor06brw@gmail.com>
2020-07-09 11:34:34 +0200

    Fix changing color

--- theme.js
+++ theme.js
@@ -1,42 +1,65 @@
 const theme = document.getElementById('themeAppButton'),
       textLight = document.getElementsByClassName('text-light'),
       inputs = document.getElementsByClassName('form-control'),
-      body = document.getElementsByTagName('body');
+      body = document.getElementsByTagName('body'),
+      inputGroupText = document.getElementsByClassName('input-group-text');
+
+
+function changeTheme() {
+    textDark = document.getElementsByClassName('text-dark');
+    [...textDark].forEach(el => {
+        el.classList.remove('text-dark');
+        el.classList.add('text-light');
+    })
+}
 
 
 theme.addEventListener('click', () => { 
-    // textLight.forEach(element => {
-        
-    // });
-    [...textLight].forEach(el => { 
-        if(el.classList.contains('text-light')) {
+    if(textLight.length > 10) {
+        [...textLight].forEach(el => {
             el.classList.remove('text-light');
             el.classList.add('text-dark');
-        } else {
+        });
+    } else {
+        textDark = document.getElementsByClassName('text-dark');
+        [...textDark].forEach(el => {
             el.classList.remove('text-dark');
             el.classList.add('text-light');
-        }
-    });
+        })
+    }
     [...inputs].forEach(el => {
-        if(el.classList.contains('form-control')) {
+        if(el.classList.contains('bg-white')) {
             el.classList.add('bg-dark');
-            el.classList.remove('bg-light');
-            el.classList.add('text-light')
+            el.classList.remove('bg-white');
+            el.classList.add('text-white')
             el.classList.remove('text-dark');
         } else {
             el.classList.remove('bg-dark');
-            el.classList.add('bg-light');
+            el.classList.add('bg-white');
             el.classList.add('text-dark')
-            el.classList.remove('text-light')
+            el.classList.remove('text-white')
         }
     });
     [...body].forEach(el => {
         if(el.classList.contains('bg-gradient')) {
             el.classList.remove('bg-gradient')
-            el.classList.add('bg-light');
+            el.classList.add('bg-white');
         } else {
             el.classList.add('bg-gradient')
-            el.classList.remove('bg-light');
+            el.classList.remove('bg-white');
         }
-    })   
+    });
+    [...inputGroupText].forEach(el => {
+        if(el.classList.contains('bg-white')) {
+            el.classList.remove('bg-white');
+            el.classList.add('bg-dark');
+            el.classList.remove('text-dark');
+            el.classList.add('text-white');
+        } else {
+            el.classList.remove('bg-dark');
+            el.classList.add('bg-white');
+            el.classList.remove('text-white');
+            el.classList.add('text-dark');
+        }
+    }); 
 })

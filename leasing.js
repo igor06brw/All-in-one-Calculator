5a37a0db1a0d5c63a7d931739be6a1839befee67
Igor Borowski <igor06brw@gmail.com>
2020-08-03 05:40:41 +0200

    Create leasing.js & collect all necessary id & change little leasing.html in id

---
+++ leasing.js
@@ -0,0 +1,33 @@
+headerTitle = document.getElementById('headerTitle');
+headerDescription = document.getElementById('headerDescription');
+
+dropdownMenuButton = document.getElementById('dropdownMenuButton');
+translateAppButton = document.getElementById('translateAppButton');
+themeAppButton = document.getElementById('themeAppButton');
+mainMenuButton = document.getElementById('mainMenuButton');
+
+leasingNetPriceInput = document.getElementById('leasingNetPriceInput');
+leasingNetPriceRange = document.getElementById('leasingNetPriceRange');
+
+leasingFirstPaymentInput = document.getElementById('leasingFirstPaymentInput');
+leasingFirstPaymentRange = document.getElementById('leasingFirstPaymentRange');
+
+leasingTermInput = document.getElementById('leasingTermInput');
+leasingTermRange = document.getElementById('leasingTermRange');
+typeOfTerm = document.getElementById('typeOfTerm');
+forYearsOfTerm = document.getElementById('forYearsOfTerm');
+forMonthsOfTerm = document.getElementById('forMonthsOfTerm');
+
+leasingRedeemPriceInput = document.getElementById('leasingRedeemPriceInput');
+leasingRedeemPriceRange = document.getElementById('leasingRedeemPriceRange');
+
+leasingInterestRateInput = document.getElementById('leasingInterestRateInput');
+leasingInterestRateRange = document.getElementById('leasingInterestRateRange');
+
+radiosOfTypePayments = document.getElementById('radiosOfTypePayments');
+forFixedPayment = document.getElementById('forFixedPayment');
+forDescreasingPayment = document.getElementById('forDescreasingPayment');
+
+displayNextPaymentResult.getElementById('displayNextPaymentResult');
+displayLastPaymentResult.getElementById('displayLastPaymentResult');
+displayFixedPaymentResult.getElementById('displayFixedPaymentResult');

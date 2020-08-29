dd95dec835b275bf73edf03dedb431eb72dcff0f
Igor Borowski <igor06brw@gmail.com>
2020-08-29 18:28:19 +0200

    fix variables

---
+++ cash-credit.js
@@ -0,0 +1,14 @@
+let cashCreditLoanInput = document.getElementById('cashCreditLoanInput'),
+    cashCreditLoanRange = document.getElementById('cashCreditLoanRange'),
+    cashCreditTermInput = document.getElementById('cashCreditTermInput'),
+    cashCreditTermRange = document.getElementById('cashCreditTermRange'),
+    cashCreditInterestRateInput = document.getElementById('cashCreditInterestRateInput'),
+    cashCreditInterestRateRange = document.getElementById('cashCreditInterestRateRange'),
+    displayFixedPaymentResult = document.getElementById('displayFixedPaymentResult');
+
+    valueFromCashCreditLoan,
+    valueFromCashCreditTerm,
+    valueFromCashCreditInterestRat;
+
+
+
